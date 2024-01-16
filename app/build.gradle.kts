e4ba63e93f00227691bbda189fcde843d68b0193
Oscar <osm1019@gmail.com>
2024-01-15 22:11:29 -0800

    Fix Up! More Alpha renaming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.crdroid.updater"
+        applicationId = "com.alpha.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
